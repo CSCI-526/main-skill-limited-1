766f2bf5ebc4e82a9eb8d12c62d1df2665dcba66
wayneclub <me@wayneclub.com>
2025-10-24 00:05:36 -0700

    Update Reward Scene

--- DiceRogue/Assets/Scripts/Reward/RewardSceneManager.cs
+++ DiceRogue/Assets/Scripts/Reward/RewardSceneManager.cs
@@ -50,22 +50,10 @@ public class RewardSceneManager : MonoBehaviour
         SceneManager.LoadScene("BattleScene");
     }
 
-    private List<BaseDice> GenerateDefaultDicePool()
-    {
-        return new List<BaseDice>
-        {
-            new BigOne(), new BigSix(), new CounterDice(), new EvenDice(), new OddDice(),
-            new HeavyDice(), new LightDice(), new MirrorDice(),
-            new CollectorDice(), new LuckySix(), new PlusOne(), new SevenSevenSeven(),
-            new TwinBond(), new WeightedEdge(),
-            new D8(), new GoldenDice(), new ZombieDice()
-        };
-    }
-
     void Start()
     {
         // 1) Build a pool (default prototypes) and pick 3 non-filler dice
-        allDicePool = GenerateDefaultDicePool();
+        allDicePool = DicePool.GetAll();
         GenerateRewardOptions();   // fills rewardResult with 3 random non-filler dice
 
         // 2) Render UI cards
